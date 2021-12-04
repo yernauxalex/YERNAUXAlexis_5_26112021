765a9f3f54fce18f4f03eb988cb5c81d29177ef0
Alexis <yernauxalex@gmail.com>
2021-12-04 14:16:58 +0100

    Optimisation script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,9 @@
 console.log(localStorage)
-
-
-// On vérifie si les données de l'api sont déjà présentes dans le localStorage
-if (localStorage.getItem("inventory")){
-    let data = []; 
+// Définition de la fonction qui affiche le contenu du localStorage
+let data = [];
+let showProduct= () => {
+    let items = document.getElementById("items");
     data = JSON.parse(localStorage.getItem("inventory"));
-    console.log("Inventaire présent dans localStorage");
-    console.log(localStorage);
-    let showProduct= () => {
-        let items = document.getElementById("items");
-            data.forEach((print, p) => {
-                items.innerHTML += `
-                <a href="./product.html?id=${data[p]._id}">
-                    <article>
-                    <img src="${data[p].imageUrl}" alt="${data[p].altTxt}"></img>
-                    <h3 class="productName">${data[p].name}</h3>
-                    <p class="productDescription">${data[p].description}</p></>
-                </article>
-            </a>`;
-            });
-    }
-    showProduct();
-    /*(function() {
-        let items = document.getElementById("items");
         data.forEach((print, p) => {
             items.innerHTML += `
             <a href="./product.html?id=${data[p]._id}">
@@ -33,25 +14,27 @@ if (localStorage.getItem("inventory")){
             </article>
         </a>`;
         });
-    })();*/
+}
+
+// On vérifie si les données de l'api sont déjà présentes dans le localStorage
+if (localStorage.getItem("inventory")){
+    console.log("Inventaire présent dans localStorage");
+    showProduct();
 }
 else {
     // On récupère les données de l'api pour les stocker dans le localStorage
     console.log("Tentative de connexion à l'api");
     (async () => {
         try {
-            console.log("Before")
             let response = await fetch ("http://localhost:3000/api/products");
             let inventoryRaw = await response.json();
             localStorage.setItem('inventory', JSON.stringify(inventoryRaw));
-            //Revoir l'appel de fonction lors du 1er chargement de la page 
-            await showProduct();
+            showProduct();
         }
         catch (e){
             console.error("Erreur d'accès à l'API");
         }
     })()
-
 }
 
 
