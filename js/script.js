26555670d50781d653bc2b0d78c781abff92d83c
Alexis <yernauxalex@gmail.com>
2021-12-04 15:24:08 +0100

    Optimisation script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
-console.log(localStorage)
+(async () => {
+    try {
+        console.log(localStorage)
+        //On récupère les données de l'api pour les stocker dans le localStorage si elles n'y sont pas présentes
+        if (!localStorage.getItem("inventory")){
+            console.log("Accès à l'api");
+            let response = await fetch ("http://localhost:3000/api/products");
+            let inventoryRaw = await response.json();
+            localStorage.setItem('inventory', JSON.stringify(inventoryRaw));
+        }
+        let data = await JSON.parse(localStorage.getItem("inventory"));
+        // Fonction qui affiche le contenu du localStorage
+        (showProduct = () => {
+            let items = document.getElementById("items");
+            data.forEach((print, p) => {
+            items.innerHTML += `
+                <a href="./product.html?id=${data[p]._id}">
+                    <article>
+                    <img src="${data[p].imageUrl}" alt="${data[p].altTxt}"></img>
+                    <h3 class="productName">${data[p].name}</h3>
+                    <p class="productDescription">${data[p].description}</p></>
+                    </article>
+                </a>`;
+            });
+        })()
+    }
+    catch (e){
+        console.error("Erreur d'accès à l'API");
+    }
+})()
+
 // Définition de la fonction qui affiche le contenu du localStorage
-let data = [];
+/*let data = [];
 let showProduct= () => {
     let items = document.getElementById("items");
     data = JSON.parse(localStorage.getItem("inventory"));
@@ -35,7 +65,7 @@ else {
             console.error("Erreur d'accès à l'API");
         }
     })()
-}
+}*/
 
 
 //Partie fonctionnelle mais pas opti car appel de l'api à chaque actualisation de la page
