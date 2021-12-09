addad3d5458af10111aefefb0bfd1bcc6fa83f79
Alexis <yernauxalex@gmail.com>
2021-12-09 15:05:30 +0100

    Suppression commentaires obsolètes

--- js/cart.js
+++ js/cart.js
@@ -330,93 +330,10 @@
         //Affichage de l'id de la commande dans la page confirmation
         if (document.URL.includes("confirmation.html")) {
             let orderId = new URL(window.location.href).searchParams.get("id");
-            document.querySelector("#orderId").innerHTML = orderId;
+            document.getElementById("orderId").innerHTML = orderId;
         }
     }
     catch (error){
         console.error(error);
     }
 })();
-
-/*
-let products = [];
-fetch("http://localhost:3000/api/products")
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
-        }
-    })
-    .then((data) => {
-        // Trouve l'objet correspondant à l'id via l'api
-        let findObject = id => {
-            let i = 0;
-            while (i <= data.length) {
-                if (data[i]._id === id) {
-                    return data[i];
-                    break;
-                }
-                i++;
-            }
-        }
-
-        // Affiche tout les produits présent dans le panier
-        let showCart = () => {
-            for (let i in productLocalStorage) {
-                let id = productLocalStorage[i]._id;
-                let productObject = findObject(id);
-                let price = productObject.price * parseInt(productLocalStorage[i].qty);
-                
-                // Stockage des id produits
-                let productsId = [productLocalStorage[i]._id];
-                products.push(productsId);
-
-                // Insertion dans la page 
-                let cart = document.getElementById("cart__items");
-                cart.innerHTML += `
-                        <article class="cart__item" data-id="${productLocalStorage[i]._id}" data-color="${productLocalStorage[i].colors}">
-                            <div class="cart__item__img">
-                                <img src="${productObject.imageUrl}" alt="${productObject.altTxt}">
-                            </div>
-                            <div class="cart__item__content">
-                                <div class="cart__item__content__description">
-                                    <h2>${productObject.name}</h2>
-                                    <p>${productLocalStorage[i].colors}</p>
-                                    <p>${price}€</p>
-                                </div>
-                                <div class="cart__item__content__settings">
-                                    <div class="cart__item__content__settings__quantity">
-                                        <p>Qté : </p>
-                                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productLocalStorage[i].qty}">
-                                    </div>
-                                    <div class="cart__item__content__settings__delete">
-                                    <p class="deleteItem">Supprimer</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </article>`
-            }
-        }
-
-        // Supprimer un produit 
-        let deleteProduct = () => {
-            
-        }
-
-        // Modifier la quantité d'un produit 
-        let modifyProduct = () => {
-
-        }
-
-        let totalcost = () => {
-
-        }
-
-        showCart();
-        // deleteProduct();
-        // modifyProduct();
-        // totalcost();
-    })
-    .catch(function (err) {
-        console.error("Erreur d'accès à l'API");
-    });
-*/
--- js/product.js
+++ js/product.js
@@ -32,48 +32,6 @@ console.log(idProduct);
         console.error(error);
     }
 })()
-/*
-fetch("http://localhost:3000/api/products")
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
-        }
-    })
-    .then((data) => {
-        //Récupère les éléments correspondant à l'id du produit via l'api
-        let i = 0;
-        let img, name, price, description, altTxt, colors;
-        while (i <= data.length) {
-            if (data[i]._id === idProduct) {
-                img = data[i].imageUrl;
-                name = data[i].name;
-                price = data[i].price;
-                description = data[i].description;
-                altTxt = data[i].altTxt;
-                colors = data[i].colors;
-                break;
-            }
-            else {
-                i++;
-            }
-        }
-
-        //Insertion des éléments dans la page
-        let baliseImg = document.getElementsByClassName("item__img");
-        baliseImg[0].innerHTML = `<img src="${img}" alt="${altTxt}">`;
-        document.getElementById("title").innerHTML = name;
-        document.getElementById("price").innerHTML = price;
-        document.getElementById("description").innerHTML = description;
-        console.log(colors)
-        let selectColor = document.getElementById("colors");
-        for (let k in colors) {
-            selectColor.insertAdjacentHTML('beforeend', `<option value="${colors[k]}">${colors[k]}</option>`);
-        }
-
-    })
-    .catch(function (err) {
-        console.error("Erreur d'accès à l'API");
-    });*/
 
 // Gestion du localStorage
 // Création d'un produit
