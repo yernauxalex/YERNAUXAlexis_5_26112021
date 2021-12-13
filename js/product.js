823c0d94d5a99d7343f6c2f7661c929736f2824e
Alexis <yernauxalex@gmail.com>
2021-12-13 12:11:01 +0100

    Ajout utils.js + fix

--- js/product.js
+++ js/product.js
@@ -1,16 +1,4 @@
 (async () => {
-    async function fetchProduct() {
-        console.log(localStorage)
-        //On récupère les données de l'api pour les stocker dans le localStorage si elles n'y sont pas présentes
-        if (!localStorage.getItem("inventory")) {
-            console.log("Accès à l'api");
-            let response = await fetch("http://localhost:3000/api/products");
-            let inventoryRaw = await response.json();
-            localStorage.setItem('inventory', JSON.stringify(inventoryRaw));
-        }
-        let data = await JSON.parse(localStorage.getItem("inventory"));
-        return data;
-    }
     // Gestion du localStorage    
     // Création d'un produit
     let createProduct = () => {
