d0ed8e0354de6b5cce147b561fac0212b6c88605
Alexis <yernauxalex@gmail.com>
2021-12-16 09:27:09 +0100

    Fix script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 (async () => {
+    // Définition de variable pour la fonction
+    let data;
+    // Fonction qui affiche le contenu du localStorage
+    showProduct = () => {
+        let items = document.getElementById("items");
+        data.forEach((print) => {
+            items.innerHTML += `
+            <a href="./product.html?id=${print._id}">
+                <article>
+                <img src="${print.imageUrl}" alt="${print.altTxt}"></img>
+                <h3 class="productName">${print.name}</h3>
+                <p class="productDescription">${print.description}</p></>
+                </article>
+            </a>`;
+        });
+    };
     try {
-        let data = await fetchProduct();
-        console.log(data);
-        // Fonction qui affiche le contenu du localStorage
-        (showProduct = () => {
-            let items = document.getElementById("items");
-            data.forEach((print, p) => {
-                items.innerHTML += `
-                <a href="./product.html?id=${data[p]._id}">
-                    <article>
-                    <img src="${data[p].imageUrl}" alt="${data[p].altTxt}"></img>
-                    <h3 class="productName">${data[p].name}</h3>
-                    <p class="productDescription">${data[p].description}</p></>
-                    </article>
-                </a>`;
-            });
-        })();
+        data = await fetchProduct();
+        showProduct();
     }
     catch (error) {
         console.error(error);
