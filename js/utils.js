254d3c5b2380e30b511bb9208b53e07fafd2f545
Alexis <yernauxalex@gmail.com>
2021-12-16 12:51:10 +0100

    Suppression console.log

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,6 @@
 async function fetchProduct() {
-    console.log(localStorage)
     //On récupère les données de l'api pour les stocker dans le localStorage si elles n'y sont pas présentes
     if (!localStorage.getItem("inventory")) {
-        console.log("Accès à l'api");
         const response = await fetch("http://localhost:3000/api/products");
         const inventoryRaw = await response.json();
         localStorage.setItem('inventory', JSON.stringify(inventoryRaw));
